37992b262977b3ea183549cd382d7d3923b459b0
Marcos Lins <vinicius.gzm0@gmail.com>
2022-10-31 22:05:17 -0300

    Modelo da PokeAPI convertido

--- Páginas Interativas com JavaScript/6 - Projeto Listagem de Pokémon/assets/js/main.js
+++ Páginas Interativas com JavaScript/6 - Projeto Listagem de Pokémon/assets/js/main.js
@@ -2,17 +2,17 @@ const offset = 0;
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+
 function convertPokemonToLi(pokemon) {
     return `
-    <li class="pokemon">
-        <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
         <span class="name">${pokemon.name}</span>
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${pokemon.types.map((type) => `<li class="type">${type.name}</li>`).join('')}
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+            <img src="${pokemon.photo}" alt="${pokemon.name}">
         </div>
     </li>
     `
