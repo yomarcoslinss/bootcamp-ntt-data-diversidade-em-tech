4aedaf2ff6a093636c6a873798bfb8dd561dd8fa
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-03 10:33:48 -0300

    correção script

--- Desafios de Projeto/trilha-css-desafio-01/assets/js/script.js
+++ Desafios de Projeto/trilha-css-desafio-01/assets/js/script.js
@@ -1,7 +1,3 @@
 function inscricaoBootcamp() {
     window.open("https://web.dio.me/track/formacao-css-web-developer");
 }
-
-function teste () {
-    window.open("https://web.dio.me/track/formacao-css-web-developer");
-}
