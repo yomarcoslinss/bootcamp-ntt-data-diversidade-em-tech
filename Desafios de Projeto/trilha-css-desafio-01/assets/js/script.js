5b7285ca4369611d8c66ac17f43fd3cc092d9055
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-03 10:33:21 -0300

    Adicionada funcionalidade aos botões

---
+++ Desafios de Projeto/trilha-css-desafio-01/assets/js/script.js
@@ -0,0 +1,7 @@
+function inscricaoBootcamp() {
+    window.open("https://web.dio.me/track/formacao-css-web-developer");
+}
+
+function teste () {
+    window.open("https://web.dio.me/track/formacao-css-web-developer");
+}
