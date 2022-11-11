7b13b35974fbcf7252997bd8d38dd15cba5ff358
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-10 22:13:43 -0300

    added

--- Desafios de Projeto/angular-blog/src/app/components/big-card/big-card.component.ts
+++ Desafios de Projeto/angular-blog/src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css', './big-card.responsive.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = "";
+  cardTitle:string = "";
+  cardDescription:string = "";
 
   constructor() { }
 
