c1d25ea1416d1287341746c933714486d96e2da6
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-11 21:29:23 -0300

    content created

---
+++ Desafios de Projeto/angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+    {
+        "id": "0",
+        "photoCover": "https://geekdama.com.br/wp-content/uploads/2021/03/one-piece-manga-1008-big-mom-kaido-colorizacao-by-monkeyd_rock-postcover.jpg",
+        "title": "Kaido e Bigmom formam uma aliança pirata",
+        "description": "Após Yonkous sofrerem atentados dos piratas supernovas, eles formam uma aliança para revidarem."
+    },
+    {
+        "id": "1",
+        "photoCover": "https://nerdhits.com.br/wp-content/uploads/2022/11/one-piece-Aokiji-barba-negra.jpg",
+        "title": "Ex-almirante se torna membro dos Piratas BN",
+        "description": "Ex-almirante Aokiji Kuzan entra para o Bando do Barba Negra e junto com outro tripulante, sequestra uma das filhas da Yonkou BigMom"
+    },
+    
+]
