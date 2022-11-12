2691a8b17ff881f925875d9e883e9e7db755d341
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-12 18:26:49 -0300

    first routes created

--- Framework Angular/7 - Rotas/rotas-angular/src/app/app-routing.module.ts
+++ Framework Angular/7 - Rotas/rotas-angular/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexModule } from './pages/index/index.module';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // caminho • Obs : Definir o caminho como vazio, irá torna-lo como página padrão do site
+    component: TitleComponent ,
+    pathMatch: 'full', // full: quando eu quero a URL EXATA !   prefix: quando eu quero que a url contenha o prefixo e outras coisas depois. 
+  },
+  {
+    path: 'portifolio',
+    component: CardComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
