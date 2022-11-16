38ed41121af889d3520c1f2cd33ce11dfc69fd68
Marcos Lins <vinicius.gzm0@gmail.com>
2022-11-16 18:25:15 -0300

    subscribe changed

--- Framework Angular/8 - Services/services-angular/src/app/components/card/card.component.ts
+++ Framework Angular/8 - Services/services-angular/src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonData } from 'src/app/models/pokemonData';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -6,12 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  name:string = 'Bulbassaur'
+  pokemon?:PokemonData | any
   attributesTypes:string[] = ['Fire', 'Stone']
 
-  constructor() { }
+  constructor(private service:PokemonService) { }
 
   ngOnInit(): void {
+    this.service.getPokemon("charizard").subscribe(
+      {
+        next: (res) => {
+
+          this.pokemon = {
+            id: res.id,
+            name: res.name,
+            sprites: res.sprites,
+            types: res.types
+          }
+
+          console.log(res)
+          console.log(this.pokemon)
+        
+        
+        
+        },
+        error: (err) => console.log(err)
+      }
+    )
   }
 
 }
---
+++ Framework Angular/8 - Services/services-angular/src/app/models/pokemonData.ts
@@ -0,0 +1,9 @@
+export type PokemonData = {
+    name:string
+    id:number
+    sprites: {
+        front_default:string
+    }
+
+    types: []
+}
